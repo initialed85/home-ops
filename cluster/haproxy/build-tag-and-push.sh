974b9e7011824b5dd2d65631420f4f0a8b3b5b5a
Edward Beech <initialed85@gmail.com>
2025-03-04 20:33:49 +0800

    WIP @ Tue  4 Mar 2025 20:33:49 AWST

--- cluster/haproxy/build-tag-and-push.sh
+++ cluster/haproxy/build-tag-and-push.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-docker build --platform=linux/amd64 -t kube-registry:5000/haproxy -f ./Dockerfile .
+# docker build --platform=linux/amd64 -t kube-registry:5000/haproxy -f ./Dockerfile .
+# docker image push --platform=linux/amd64 kube-registry:5000/haproxy
 
-docker image push --platform=linux/amd64 kube-registry:5000/haproxy
+docker build --platform=linux/amd64 -t initialed85/haproxy -f ./Dockerfile .
+docker image push --platform=linux/amd64 initialed85/haproxy
--- cluster/keepalived/build-tag-and-push.sh
+++ cluster/keepalived/build-tag-and-push.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-docker build --platform=linux/amd64 -t kube-registry:5000/keepalived -f ./Dockerfile .
+# docker build --platform=linux/amd64 -t kube-registry:5000/keepalived -f ./Dockerfile .
+# docker image push --platform=linux/amd64 kube-registry:5000/keepalived
 
-docker image push --platform=linux/amd64 kube-registry:5000/keepalived
+docker build --platform=linux/amd64 -t initialed85/keepalived -f ./Dockerfile .
+docker image push --platform=linux/amd64 initialed85/keepalived
